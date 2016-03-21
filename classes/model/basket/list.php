3ca9d7a26869a7672dc744c054554f9195c34a34
greor <gregor.zoonoid@gmail.com>
2016-03-21 17:07:01 +0300

    init
    Signed-off-by: greor <gregor.zoonoid@gmail.com>

---
+++ classes/model/basket/list.php
@@ -0,0 +1,70 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Basket_List extends ORM_Base {
+
+	protected $_table_name = 'catalog_basket_list';
+	protected $_sorting = array('basket_id' => 'ASC');
+	protected $_deleted_column = 'delete_bit';
+
+	protected $_belongs_to = array(
+		'basket' => array(
+			'model' => 'basket',
+			'foreign_key' => 'basket_id',
+		),
+		'nomenclature' => array(
+			'model' => 'nomenclature',
+			'foreign_key' => 'nomenclature_id',
+		),
+	);
+	
+	public function labels()
+	{
+		return array(
+			'basket_id' => 'Basket',
+			'nomenclature_id' => 'Product',
+			'name' => 'Name',
+			'count' => 'Count',
+			'price' => 'Price',
+		);
+	}
+
+	public function rules()
+	{
+		return array(
+			'id' => array(
+				array('digit'),
+			),
+			'basket_id' => array(
+				array('not_empty'),
+				array('digit'),
+			),
+			'nomenclature_id' => array(
+				array('not_empty'),
+				array('digit'),
+			),
+			'name' => array(
+				array('not_empty'),
+				array('max_length', array(':value', 255)),
+			),
+			'count' => array(
+				array('not_empty'),
+				array('max_length', array(':value', 255)),
+			),
+			'price' => array(
+				array('not_empty'),
+				array('max_length', array(':value', 255)),
+			),
+		);
+	}
+
+	public function filters()
+	{
+		return array(
+			TRUE => array(
+				array('trim'),
+				array('strip_tags'),
+			),
+		);
+	}
+	
+}
